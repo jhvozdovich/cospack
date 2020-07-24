dd0f5381b4d939b8958fca7b57c04fd411058c94
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-24 10:02:09 -0700

    add home screen buttons for future navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 "use strict"
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./Home";
@@ -11,21 +11,28 @@ import EventList from "./events/EventList";
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="CosPack" component={Home} />
-        <Stack.Screen name="CosplayList" component={CosplayList} options={{ title: "Cosplays" }} />
+      <Stack.Navigator initialRouteName="CosplayPack">
+        <Stack.Screen name="CosPack" component={Home} options={{
+          title: "CosPack",
+          headerRight: () => (
+            <React.Fragment>
+              <Button
+                onPress={() => alert("Settings here!")}
+                title="Settings"
+              />
+              <Button
+                onPress={() => alert("Login here!")}
+                title="Login"
+              />
+            </React.Fragment>
+          )
+        }} />
+        < Stack.Screen name="CosplayList" component={CosplayList} options={{ title: "Cosplays" }} />
         <Stack.Screen name="EventList" component={EventList} options={{ title: "Events" }} />
       </Stack.Navigator>
     </NavigationContainer >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-});
 
 const Stack = createStackNavigator();
 
